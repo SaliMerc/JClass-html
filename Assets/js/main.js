aacecb6d5e0a463a8bdcc3973b65c86485d3d94d
SaliMerc <nyawadesaline@gmail.com>
2024-10-16 15:35:59 +0300

    Initial commit

---
+++ Assets/js/main.js
@@ -0,0 +1,7 @@
+// get the button element
+const cButton=document.getElementById('colorButton');
+// add the event listener for the button
+cButton.addEventListener('click',()=>{
+    // cButton.style.backgroundColor=cButton.style.backgroundColor==='yellow' ? 'blue':'yellow';
+    cButton.style.backgroundColor=cButton.style.backgroundColor==='brown' ? 'blue':'brown';
+})
