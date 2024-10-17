fd224406e4e1418b770889e8a4aff387703f29e8
SaliMerc <nyawadesaline@gmail.com>
2024-10-17 15:08:12 +0300

    added file

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,7 +1,10 @@
 // get the button element
 const cButton=document.getElementById('colorButton');
+const afterMessage=document.getElementById('after');
 // add the event listener for the button
 cButton.addEventListener('click',()=>{
     // cButton.style.backgroundColor=cButton.style.backgroundColor==='yellow' ? 'blue':'yellow';
     cButton.style.backgroundColor=cButton.style.backgroundColor==='brown' ? 'blue':'brown';
+    cButton.style.color=cButton.style.color==='red'?'black' : 'red';
+    afterMessage.style.display='block';
 })
